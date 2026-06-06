d79a51a78b91e5fa9f51fa3946e7e743f9f9a8cd
RAYAN BAYDIR <rayan@iMac-de-RAYAN.local>
2026-06-06 23:15:39 +0200

    fix bot defined error

--- degat.py
+++ degat.py
@@ -12,6 +12,14 @@ from discord.ext import commands
 import asyncio
 import aiohttp
 import os
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Bot connecté : {bot.user}")
 bot.run(os.getenv("TOKEN"))
 
 intents = discord.Intents.default()
